653173b5c86469eec7222e019200bdfae4b11193
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-11-24 21:38:57 +0600

    doteenvupdated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
