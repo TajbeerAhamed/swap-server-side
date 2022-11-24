52acc39e17e8fdb004b6ec6d76ca20ff92da2526
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-11-24 21:39:40 +0600

    middlewares added

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middle Wares //
-app.use(cors());
-app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('Swap Server site is Running')
