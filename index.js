15be6cefe6b2f971ad2a90315251579c1aa5ad66
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-11-24 21:40:09 +0600

    cors added

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middle Wares //
-
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Swap Server site is Running')
