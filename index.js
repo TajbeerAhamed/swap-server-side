675be09588e5673c8e1ff7e8b3da44421313e21e
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-11-24 21:42:43 +0600

    server connected to db

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.get('/', (req, res) => {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rjzdusk.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
